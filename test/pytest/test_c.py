ff0b270518b6fd5def89d45242514a0e9b16a0f6
no7dw <no7david@gmail.com>
2018-02-28 09:02:30 +0800

    add pytest setup teardown

---
+++ test/pytest/test_c.py
@@ -0,0 +1,23 @@
+import pytest
+
+def square(i):
+    return i*i
+
+class TestClass:
+    def setup_class(self):
+        # print("setup_class")
+        pass
+        
+    def teardown_class(self):
+        # print("teardown_class")
+        pass
+
+    @pytest.mark.parametrize("data, result", [
+        (6, 36),
+        (3, 9),
+    ])        
+    def test_square(self, data, result):
+        print("abc")
+        assert result == square(data)
+
+            
