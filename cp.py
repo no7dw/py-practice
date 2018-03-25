0066a58f80860f00eac4c3be25c6ef031db22172
no7dw <no7david@gmail.com>
2018-03-25 23:25:48 +0800

    add link and keng summary

--- cp.py
+++ cp.py
@@ -1,8 +1,8 @@
 import copy
 print("python is pass by reference")
 a=[[1],[2],[3]]
-b=copy.copy(a)
-c=copy.deepcopy(a) # I would not change if a change
+b=copy.copy(a) # copy 简单对象
+c=copy.deepcopy(a) # I would not change if a change copy 复杂子对象(object)
 
 print("before","==>")
 print("a",a)
@@ -18,3 +18,5 @@ print("after","==>")
 print("a",a)
 print("b",b)
 print("c",c)
+
+[ref link](https://iaman.actor/blog/2016/04/17/copy-in-python)
