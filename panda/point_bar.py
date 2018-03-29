10cb797f936dc94475833ed7c022267eaf518b4e
no7dw <no7david@gmail.com>
2018-03-29 11:19:01 +0800

    refactor#change def

--- panda/point_bar.py
+++ panda/point_bar.py
@@ -10,23 +10,26 @@ col_ass = "Assignee"
 from_file = "storypoint.csv"
 to_file = "new_storypoints.csv"
 
-df=pd.read_csv(from_file)
-l=df.loc[0:, [col_point, col_proj, col_ass]]
-f=l.groupby(col_ass).sum()
-fn=f.dropna()
+def run_draw():
+    df=pd.read_csv(from_file)
+    l=df.loc[0:, [col_point, col_proj, col_ass]]
+    f=l.groupby(col_ass).sum()
+    fn=f.dropna()
 
-fn.to_csv(to_file)
-pd.read_csv(to_file)
-f=pd.read_csv(to_file)
+    fn.to_csv(to_file)
+    pd.read_csv(to_file)
+    f=pd.read_csv(to_file)
 
-# choose x,y
-y=np.array(f[col_point])
-x=np.array(f[col_ass])
-#draw bar char
-plt.figure(figsize=(20,10))
-plt.bar(x,y)
-plt.show()
-#save file
-fig = plt.gcf()
-fig.savefig('result.png')
+    # choose x,y
+    y=np.array(f[col_point])
+    x=np.array(f[col_ass])
+    #draw bar char
+    plt.figure(figsize=(20,10))
+    plt.bar(x,y)
+    #save file
+    fig = plt.gcf()
+    fig.savefig('./templates/result.png')
 
+    #plt.show()
+if __name__ == '__main__':
+    run_draw()
