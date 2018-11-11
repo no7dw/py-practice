96f43b58162041914ed3865c42425f1a9b3e6fbf
no7dw <no7david@gmail.com>
2018-11-11 21:23:38 +0800

    add passing args between thread

--- thread/basic.py
+++ thread/basic.py
@@ -1,6 +1,6 @@
 import time,threading
-def sleep_loop():
-    print("thread is running", threading.current_thread().name)
+def sleep_loop(sec, strarg):
+    print("thread is running", threading.current_thread().name,sec, strarg)
     count = 0
     while count<4:
         count = count+1
@@ -9,7 +9,8 @@ def sleep_loop():
 
     print("worker finish")
 
-t  = threading.Thread(target=sleep_loop, name='sleep_loop')
+#t  = threading.Thread(target=sleep_loop, name='sleep_loop', args=2)
+t  = threading.Thread(target=sleep_loop, name='sleep_loop', args=(2, "abcdefg",))
 t.start()
 print("main thread continue")
 print("waiting for  thread end")
