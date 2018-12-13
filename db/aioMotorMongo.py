39672fff677655dfe74de51855ea5eefe1677012
no7dw <no7david@gmail.com>
2018-12-13 22:20:48 +0800

    use async motor

--- db/aioMotorMongo.py
+++ db/aioMotorMongo.py
@@ -1,13 +1,14 @@
-import pymongo
+import motor.motor_asyncio
 import time
 import asyncio
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+
 db=client['cashLoan']
 col=db['userModelLog']
 
 async def q(query):
-    result = await col.find(query)
+    result = await col.find_one(query)
     #print(result)
     #return list(result)
     return result
@@ -15,12 +16,14 @@ async def q(query):
 async def main():
     arrjob = []
     query= {'orderId':'5bf6d577ea5140000f0dc03c'}
-    for i in list(range(0,3000)):
+    for i in list(range(0,10000)):
         arrjob.append(q(query))
     print(len(arrjob))
     dones, pendings = await asyncio.wait(arrjob)
-    
-    #print(dones[1].result())
+
+    for done in dones:    
+        pass
+        #print(done.result())
 
 s = time.time()
 loop = asyncio.get_event_loop()
--- db/mongo.py
+++ db/mongo.py
@@ -8,7 +8,7 @@ col=db['userModelLog']
 
 def q():
     query= {'orderId':'5bf6d577ea5140000f0dc03c'}
-    for i in list(range(1,3000)):
+    for i in list(range(1,10000)):
         result = list(col.find(query))
         #print(i, result[0])
 s = time.time()
