75dd4aa2b2062703a1df70dbb3f9ef6a3cb72312
no7dw <no7david@gmail.com>
2018-12-19 14:34:00 +0800

    simplify and gather result

--- async/asyncDemo.py
+++ async/asyncDemo.py
@@ -1,18 +1,30 @@
 import asyncio
 
-async def foo():
+async def foo(sec=1):
     print('in foo')
-    await asyncio.sleep(1)
+    await asyncio.sleep(sec)
     print('explicit context switch to foo again')
+    return sec
 
-async def bar():
+async def bar(sec=3):
     print('in bar')
-    await asyncio.sleep(3)
+    await asyncio.sleep(sec)
     print('implicit context switch back to bar')
+    return sec
 
-ioloop = asyncio.get_event_loop()
-tasks = [ioloop.create_task(foo()), ioloop.create_task(bar())]
+loop = asyncio.get_event_loop()
+# tasks = [loop.create_task(foo()), loop.create_task(bar())] # 简化成下方
+tasks = [foo(), bar()] 
 wait_tasks = asyncio.wait(tasks)
-ioloop.run_until_complete(wait_tasks)
-ioloop.close()
+loop.run_until_complete(wait_tasks)
+
+# 使用gather 去获取 coroutine/future result
+result = loop.run_until_complete(asyncio.gather(*[
+    foo(),
+    bar()
+]))
+for i in result:
+    print(i)
+
+loop.close()
 
