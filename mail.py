eb1327268a1ccaa7a042a2a2ba4467fba24ad741
no7dw <no7david@gmail.com>
2019-03-16 18:38:37 +0800

    refactor:update and refactor mail

--- mail.py
+++ mail.py
@@ -6,27 +6,35 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage 
 from email.header import Header   
     
+sender_name='xxx'    
+
 smtpserver = 'smtp.vip.163.com'
-username = 'sender@vip.163.com'
-password='AUTHCODE'
-sender='sender@vip.163.com'
-receiver=['receiver@126.com','receiver@gmail.com','sender@vip.163.com']
+username = sender_name+'@vip.163.com'
+# vip 163 客户授权码，非登录密码
+password='AUTHCODE' 
+sender= sender_name+'@vip.163.com'
+receiver=['yyy@gmail.com',sender_name+'@vip.163.com']
 
-subject = 'Python3 email test'
+def send(msg):
+  smtp = smtplib.SMTP()    
+  smtp.connect('smtp.vip.163.com')
+  #打印出和SMTP服务器交互的所有信息。
+  #smtp.set_debuglevel(1)  
+  smtp.login(username, password)    
+  smtp.sendmail(sender, receiver, msg.as_string())    
+  smtp.quit()
 
-msg = MIMEMultipart('mixed') 
-msg['Subject'] = subject
-msg['From'] = 'sender@vip.163.com <sender@vip.163.com>'
-msg['To'] = ";".join(receiver) 
-
-text = "Hi!\nHow are you?"    
-text_plain = MIMEText(text,'plain', 'utf-8')    
-msg.attach(text_plain)    
-
-smtp = smtplib.SMTP()    
-smtp.connect('smtp.vip.163.com')
-#打印出和SMTP服务器交互的所有信息。
-#smtp.set_debuglevel(1)  
-smtp.login(username, password)    
-smtp.sendmail(sender, receiver, msg.as_string())    
-smtp.quit()
+def construct_msg():
+  subject = 'Python3 email test'
+  msg = MIMEMultipart('mixed') 
+  msg['Subject'] = subject
+  msg['From'] = sender_name+'@vip.163.com '+ '<' + sender_name + '@vip.163.com>'
+  msg['To'] = ";".join(receiver) 
+  text = "Hi!\nHow are you?"    
+  text_plain = MIMEText(text,'plain', 'utf-8')    
+  msg.attach(text_plain)  
+  return msg  
+  
+if __name__ == '__main__':
+  msg = construct_msg()
+  send(msg)
