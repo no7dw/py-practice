1560d921b83e9d69a47f2578f6f3b53b586c5871
no7dw <no7david@gmail.com>
2019-03-24 15:08:41 +0800

    add args parse

--- args.py
+++ args.py
@@ -1,14 +1,17 @@
 import sys
-import os
+import argparse
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--coin', type=str, required=True, help='coin')
+    args = parser.parse_args()
+    print(args.coin)
 
-print("script name is", sys.argv[0])
-if len(sys.argv)>1:
-  print("there are" ,len (sys.argv)-1, "arguments:")
-  for arg in sys.argv[1:]:
-    print(arg)
-else:
-  print("there are no arguments!")
-
-for path in os.environ["PATH"].split(os.pathsep):
-  print(path)
-
+# (base) dengwei@RMBAP:~/projects/work/trading/wade/grid/stra-grid$ python a.py -c='eos'
+# eos
+# (base) dengwei@RMBAP:~/projects/work/trading/wade/grid/stra-grid$ python a.py -c 'eos'
+# eos
+# (base) dengwei@RMBAP:~/projects/work/trading/wade/grid/stra-grid$ python a.py --coin 'eos'
+# eos
+# (base) dengwei@RMBAP:~/projects/work/trading/wade/grid/stra-grid$ python a.py --coin='eos'
+# eos
