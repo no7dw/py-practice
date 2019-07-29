617c97aee9c70da2b3c301144e34d47a3db54e9d
no7dw <no7david@gmail.com>
2019-07-29 17:27:05 +0800

    add gevent

---
+++ gevent/socket-test.py
@@ -0,0 +1,23 @@
+import gevent
+import socket
+import datetime
+from  urllib  import request
+import datetime
+
+def read_web(url):
+    if url.find("https://"):
+        url = "https://" + url 
+    print('open' , url)
+    resp = request.urlopen(url)
+    data = resp.read()
+    print('%d bytes received from %s.' % (len(data), url))
+    return len(data)
+
+
+s = datetime.datetime.now()
+urls = ['www.youtube.com', 'www.jd.com', 'www.google.com', 'www.qq.com']
+jobs = [gevent.spawn(read_web, url) for url in urls]
+gevent.joinall(jobs, timeout=15)
+e= datetime.datetime.now()
+print(e-s)
+print([job.value for job in jobs]) 
